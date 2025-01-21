5d6a1d6f6b4343199ce2ede344cb45a5e944d0b6
Julien Vanier <jvanier@gmail.com>
2025-01-20 19:44:40 -0500

    Test lift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorTest.java
@@ -1,18 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-
+import com.qualcomm.robotcore.hardware.Servo;
+@Config
 @TeleOp
 public class MotorTest extends LinearOpMode {
-
+public static double POSITION_A = 0.6;
+public static double POSITION_B = 0;
     @Override
     public void runOpMode() throws InterruptedException {
-        DcMotorEx motor = hardwareMap.get(DcMotorEx.class, "motor");
-
+        Servo servo = hardwareMap.get(Servo.class, "lift");
         while(!isStopRequested()) {
-            motor.setPower(1);
+            if (gamepad1.a) {
+                servo.setPosition(POSITION_A);
+            }
+            if (gamepad1.b) {
+                servo.setPosition(POSITION_B);
+            }
         }
     }
 }
