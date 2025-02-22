7fd7c324e2496a49bc95d6cf4e18603ad0a6e4d7
Julien Vanier <jvanier@gmail.com>
2025-02-22 09:31:51 -0500

    Follow simple path

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobofestMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobofestMain.java
@@ -1,17 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.pedropathing.follower.Follower;
+import com.pedropathing.localization.Pose;
+import com.pedropathing.pathgen.BezierLine;
+import com.pedropathing.pathgen.Path;
+import com.pedropathing.pathgen.Point;
+import com.pedropathing.util.Constants;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
+
+import pedroPathing.constants.FConstants;
+import pedroPathing.constants.LConstants;
 
 @TeleOp
 @Config
 public class RobofestMain extends LinearOpMode {
+    private Follower follower;
+    public static Pose startPose = new Pose(28, 7, Math.toRadians(0));
+    public static Pose crossPose = new Pose(28, 100, Math.toRadians(0));
     private Servo claw;
     private Servo lift;
     public static double LIFT_DOWN = 0;
@@ -20,23 +29,21 @@ public class RobofestMain extends LinearOpMode {
     public static double CLAW_CLOSED = 1;
     @Override
     public void runOpMode() throws InterruptedException {
+        Constants.setConstants(FConstants.class, LConstants.class);
+        follower = new Follower(hardwareMap);
+
         claw = hardwareMap.get(Servo.class, "claw");
         lift = hardwareMap.get(Servo.class, "lift");
-        DcMotorEx frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
-        DcMotorEx frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
-        DcMotorEx backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
-        DcMotorEx backRight = hardwareMap.get(DcMotorEx.class, "backRight");
-
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
 
         TouchSensor button = hardwareMap.get(TouchSensor.class, "button");
         int state = 0;
 
         boolean oldPressed = false;
 
+        Path drive = new Path(new BezierLine(new Point(startPose), new Point(crossPose)));
 
         while(!isStopRequested()) {
+            follower.update();
             boolean pressed = button.isPressed();
             switch (state) {
                 case 0:
@@ -57,15 +64,11 @@ public class RobofestMain extends LinearOpMode {
                     }
                     break;
                 case 1:
-                    frontLeft.setPower(1);
-                    frontRight.setPower(1);
-                    backLeft.setPower(1);
-                    backRight.setPower(1);
+                    follower.followPath(drive);
+                    state = 2;
+                    break;
+                case 2:
                     if (pressed && !oldPressed) {
-                        frontLeft.setPower(0);
-                        frontRight.setPower(0);
-                        backLeft.setPower(0);
-                        backRight.setPower(0);
                         state = 0;
                     }
                     break;
