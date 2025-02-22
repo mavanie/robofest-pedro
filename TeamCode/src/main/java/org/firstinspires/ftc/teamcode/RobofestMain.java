340c337ba700e37b20455b3a6a791394f37012de
Julien Vanier <jvanier@gmail.com>
2025-02-22 11:54:39 -0500

    First successful path.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobofestMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobofestMain.java
@@ -5,7 +5,7 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.localization.Pose;
 import com.pedropathing.pathgen.BezierLine;
-import com.pedropathing.pathgen.Path;
+import com.pedropathing.pathgen.PathChain;
 import com.pedropathing.pathgen.Point;
 import com.pedropathing.util.Constants;
 import com.acmerobotics.dashboard.config.Config;
@@ -13,8 +13,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
-
-import kotlin.Unit;
 import pedroPathing.constants.FConstants;
 import pedroPathing.constants.LConstants;
 
@@ -22,8 +20,14 @@ import pedroPathing.constants.LConstants;
 @Config
 public class RobofestMain extends LinearOpMode {
     private Follower follower;
-    public static Pose startPose = new Pose(5.5, 35, Math.toRadians(0));
-    public static Pose crossPose = new Pose(55, 35, Math.toRadians(0));
+    public static Pose startPoseEast = new Pose(5.5, 14, Math.toRadians(0));
+    public static Pose startPoseWest = new Pose(5.5, 14, Math.toRadians(180));
+    public static Pose startPoseNorth = new Pose(5.5, 14, Math.toRadians(90));
+    public static Pose startPoseSouth = new Pose(5.5, 14, Math.toRadians(-90));
+    public static Pose startPose = startPoseEast;
+    public static Pose crossPose = new Pose(55, 14, Math.toRadians(0));
+    public static Pose boxPose = new Pose(30, 14, Math.toRadians(0));
+    public static Pose pickupPose = new Pose(30, 9, Math.toRadians(-90));
 
     private Servo claw;
     private Servo lift;
@@ -32,7 +36,7 @@ public class RobofestMain extends LinearOpMode {
     public static double CLAW_OPEN = 0;
     public static double CLAW_CLOSED = 1;
     @Override
-    public void runOpMode() throws InterruptedException {
+    public void runOpMode(){
         Constants.setConstants(FConstants.class, LConstants.class);
         follower = new Follower(hardwareMap);
         follower.setStartingPose(startPose);
@@ -47,13 +51,32 @@ public class RobofestMain extends LinearOpMode {
 
         boolean oldPressed = false;
 
-        Path drive = new Path(new BezierLine(new Point(startPose), new Point(crossPose)));
+        PathChain box = follower.pathBuilder()
+            .addPath(new BezierLine(new Point(startPose), new Point(boxPose)))
+            .build();
+        PathChain turn = follower.pathBuilder()
+            .addPath(new BezierLine(new Point(boxPose), new Point(boxPose)))
+            .setLinearHeadingInterpolation(boxPose.getHeading(), pickupPose.getHeading())
+            .build();
+        PathChain pickup = follower.pathBuilder()
+            .addPath(new BezierLine(new Point(boxPose), new Point(pickupPose)))
+            .build();
 
         while(!isStopRequested()) {
             follower.update();
             boolean pressed = button.isPressed();
+            if (pressed && !oldPressed) {
+                if (state == 0) {
+                    follower.followPath(box);
+                    state = 1;
+                }else{
+                    state = 0;
+                }
+            }
             switch (state) {
                 case 0:
+                    follower.breakFollowing();
+                    follower.setPose(startPose);
                     if (gamepad1.a) {
                         lift.setPosition(LIFT_DOWN);
                     }
@@ -66,25 +89,29 @@ public class RobofestMain extends LinearOpMode {
                     if (gamepad1.y) {
                         claw.setPosition(CLAW_CLOSED);
                     }
-                    if (pressed && !oldPressed) {
-                        state = 1;
-                    }
                     break;
                 case 1:
-                    follower.followPath(drive);
-                    state = 2;
+                    if (!follower.isBusy()) {
+                        follower.holdPoint(new Pose(boxPose.getX(), boxPose.getY(), pickupPose.getHeading()));
+                        state = 2;
+                    }
                     break;
                 case 2:
-                    if (pressed && !oldPressed) {
-                        state = 0;
+                    if (Math.abs(follower.headingError) < Math.toRadians(5)) {
+                        follower.followPath(pickup);
+                        state = 3;
                     }
+                    break;
+                case 3:
                     break;
             }
             oldPressed = pressed;
             telemetry.addData("state", state);
+            telemetry.addData("busy", follower.isBusy());
             telemetry.addData("X", follower.getPose().getX());
             telemetry.addData("Y", follower.getPose().getY());
             telemetry.addData("Heading", Math.toDegrees(follower.getPose().getHeading()));
+            telemetry.addData("Heading Error", Math.toDegrees(follower.headingError));
             telemetry.addData("lift", lift.getPosition());
             telemetry.addData("claw", claw.getPosition());
             telemetry.update();
